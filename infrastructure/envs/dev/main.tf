cf75766a6784f5eefef99e725514cb360432ca3e
rajendrasontyana10 <rajendra-naidu.sontyana@capgemini.com>
2026-05-12 18:43:59 +0530

    added WAF stack for prod and DR ingress

--- infrastructure/envs/dev/main.tf
+++ infrastructure/envs/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "terraform-state-dev"
+    bucket         = "terraform-state-dev-rajendra"
     key            = "infrastructure/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
