5e51f7c7205e3e880879b5601f799c795fedebd7
rajendrasontyana10 <rajendra-naidu.sontyana@capgemini.com>
2026-05-13 18:24:53 +0530

    updated setup

--- infrastructure/modules/Loadbalancer/main.tf
+++ infrastructure/modules/Loadbalancer/main.tf
@@ -14,7 +14,7 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "app" {
   name     = "app-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "app" {
 resource "aws_lb_target_group_attachment" "app" {
   target_group_arn = aws_lb_target_group.app.arn
   target_id        = var.instance_id
-  port             = 80
+  port             = 8080
 }
 
 resource "aws_lb_listener" "app" {
